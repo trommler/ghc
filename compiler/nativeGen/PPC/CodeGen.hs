ff681b8ecb850739c7191b61c47a1a53ceeecd71
Peter Trommler <ptrommler@acm.org>
2014-12-15 19:04:45 +0100

    save TOC before loading new TOC from FunDesc

--- compiler/nativeGen/PPC/CodeGen.hs
+++ compiler/nativeGen/PPC/CodeGen.hs
@@ -1073,11 +1073,12 @@ genCCall' dflags gcp target dest_regs args0
                 (dynReg, dynCode) <- getSomeReg dyn
                 case gcp of
                      GCPLinux64ELF1 -> return ( dynCode
+                               `appOL`  codeBefore
                                `snocOL` MR r12 dynReg
+                               `snocOL` LD II64 r11 (AddrRegImm r12 (ImmInt 16))
                                `snocOL` LD II64 r11 (AddrRegImm r12 (ImmInt 0))
                                `snocOL` LD II64 toc (AddrRegImm r12 (ImmInt 8))
                                `snocOL` MTCTR r11
-                               `snocOL` LD II64 r11 (AddrRegImm r12 (ImmInt 16))                               `appOL`  codeBefore
                                `snocOL` BCTRL usedRegs
                                `appOL`  codeAfter)
  
