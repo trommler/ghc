0f809eda705be4c021ae1878792d4bbe33b72ee1
Peter Trommler <ptrommler@acm.org>
2015-01-14 20:23:47 +0100

    reserve r13 only on powerpc64

--- includes/CodeGen.Platform.hs
+++ includes/CodeGen.Platform.hs
@@ -879,8 +879,9 @@ freeReg 0 = fastBool False -- Hack: r0 can't be used in all insns,
                            -- but it's actually free
 freeReg 1 = fastBool False -- The Stack Pointer
 
--- TODO: make this conditonal for ppc64 ELF
+# if powerpc64_TARGET_ARCH
 freeReg 13 = fastBool False -- reserved for system thread ID
+# endif
 
 # if !MACHREGS_darwin
 -- most non-darwin powerpc OSes use r2 as a TOC pointer or something like that
