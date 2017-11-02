trommler/ghc

commit d7b8da1f92b9ee26cd7e70028f88539e6b1ecb31

Fix broken LLVM code gen
In 8b007abbeb30 (nativeGen: Consistently use blockLbl to generate
CLabels from BlockIds) all blockLbls were changed. This interfered with
the `toInfoLbl` call in CmmProcPoint, and caused the LLVM backend to
fall over.

Reviewers: bgamari, austin, simonmar

Reviewed By: bgamari

Subscribers: rwbarton, thomie

Differential Revision: https://phabricator.haskell.org/D4006

compiler/cmm/CmmProcPoint.hs
@@ -275,8 +275,9 @@ splitAtProcPoints dflags entry_label callPPs procPoints procMap
      let add_label map pp = mapInsert pp lbls map
            where lbls | pp == entry = (entry_label, fmap cit_lbl (mapLookup entry info_tbls))
                       | otherwise   = (block_lbl, guard (setMember pp callPPs) >>
-                                                    Just (toInfoLbl block_lbl))
-                      where block_lbl = blockLbl pp
+                                                    Just info_table_lbl)
+                      where block_lbl      = blockLbl pp
+                            info_table_lbl = infoTblLbl pp
 
          procLabels :: LabelMap (CLabel, Maybe CLabel)
          procLabels = foldl' add_label mapEmpty

commit 102c238c4e92521355d3fd0868d604eeebd37e64

Fix PPC NCG after blockID patch
Summary:
Commit 8b007a assigns the same label to the first basic block
of a proc and to the proc entry point. This violates the PPC ELF
v. 1.9 ABI and leads to duplicate symbols.

This patch fixes duplicate symbols caused by block labels

In commit d7b8da1 an info table label is generated from a block id.
Getting the entry label from that info label leads to an undefined
symbol because a suffix "_entry" that is not present in the block label.

To fix that issue add a new info table label flavour for labels
derived form block ids. Converting such a label with toEntryLabel
produces the original block label.

Fixes #14311

Test Plan: ./validate

Reviewers: austin, bgamari, simonmar, erikd, hvr

Subscribers: rwbarton, thomie

GHC Trac Issues: #14311

Differential Revision: https://phabricator.haskell.org/D4149

compiler/cmm/BlockId.hs
@@ -5,7 +5,7 @@
 module BlockId
   ( BlockId, mkBlockId -- ToDo: BlockId should be abstract, but it isn't yet
   , newBlockId
-  , blockLbl, infoTblLbl
+  , blockLbl, infoTblLbl, blockInfoTblLbl
   ) where
 
 import GhcPrelude
@@ -44,3 +44,6 @@ blockLbl label = mkAsmTempLabel (getUnique label)
 
 infoTblLbl :: BlockId -> CLabel
 infoTblLbl label = mkInfoTableLabel (mkFCallName (getUnique label) "block") NoCafRefs
+
+blockInfoTblLbl :: BlockId -> CLabel
+blockInfoTblLbl label = mkBlockInfoTableLabel (mkFCallName (getUnique label) "block") NoCafRefs

compiler/cmm/CLabel.hs
@@ -35,6 +35,8 @@ module CLabel (
         mkLocalConInfoTableLabel,
         mkLocalClosureTableLabel,
 
+        mkBlockInfoTableLabel,
+
         mkReturnPtLabel,
         mkReturnInfoLabel,
         mkAltLabel,
@@ -390,6 +392,7 @@ data IdLabelInfo
 
   | Bytes               -- ^ Content of a string literal. See
                         -- Note [Bytes label].
+  | BlockInfoTable      -- ^ Like InfoTable but BlockID instead of Entry label
 
   deriving (Eq, Ord)
 
@@ -489,6 +492,8 @@ mkBytesLabel name                 = IdLabel name NoCafRefs Bytes
 mkConEntryLabel       :: Name -> CafInfo -> CLabel
 mkConEntryLabel name        c     = IdLabel name c ConEntry
 
+mkBlockInfoTableLabel :: Name -> CafInfo -> CLabel
+mkBlockInfoTableLabel name c = IdLabel name c BlockInfoTable
 -- Constructing Cmm Labels
 mkDirty_MUT_VAR_Label, mkSplitMarkerLabel, mkUpdInfoLabel,
     mkBHUpdInfoLabel, mkIndStaticInfoLabel, mkMainCapabilityLabel,
@@ -693,6 +698,7 @@ toSlowEntryLbl l = pprPanic "toSlowEntryLbl" (ppr l)
 toEntryLbl :: CLabel -> CLabel
 toEntryLbl (IdLabel n c LocalInfoTable)  = IdLabel n c LocalEntry
 toEntryLbl (IdLabel n c ConInfoTable)    = IdLabel n c ConEntry
+toEntryLbl (IdLabel n _ BlockInfoTable)  = mkAsmTempLabel (nameUnique n)
 toEntryLbl (IdLabel n c _)               = IdLabel n c Entry
 toEntryLbl (CaseLabel n CaseReturnInfo)  = CaseLabel n CaseReturnPt
 toEntryLbl (CmmLabel m str CmmInfo)      = CmmLabel m str CmmEntry
@@ -1263,6 +1269,7 @@ ppIdFlavor x = pp_cSEP <>
                        ConInfoTable     -> text "con_info"
                        ClosureTable     -> text "closure_tbl"
                        Bytes            -> text "bytes"
+                       BlockInfoTable   -> text "info"
                       )
 
 

compiler/cmm/CmmProcPoint.hs
@@ -277,7 +277,7 @@ splitAtProcPoints dflags entry_label callPPs procPoints procMap
                       | otherwise   = (block_lbl, guard (setMember pp callPPs) >>
                                                     Just info_table_lbl)
                       where block_lbl      = blockLbl pp
-                            info_table_lbl = infoTblLbl pp
+                            info_table_lbl = blockInfoTblLbl pp
 
          procLabels :: LabelMap (CLabel, Maybe CLabel)
          procLabels = foldl' add_label mapEmpty