3b3132019eda837934662175e3daa360499e7f4b
Peter Trommler <ptrommler@acm.org>
2023-05-07 13:06:47 +0200

    Fix recom015 on openSUSE

--- testsuite/tests/driver/recomp015/Generate.hs
+++ testsuite/tests/driver/recomp015/Generate.hs
@@ -1,7 +1,9 @@
 import Control.Monad (forM_)
 
 main :: IO ()
-main = forM_ [0..0xffff] $ \i -> do
+main = do
+  forM_ [0..0xffff] $ \i -> do
    putStrLn $ ".section s" ++ show i ++ ",\"\",@progbits"
    putStrLn $ ".asciz \"Section " ++ show i ++ "\""
    putStrLn ""
+  putStrLn ".section .note.GNU-stack,\"\",%progbits"
